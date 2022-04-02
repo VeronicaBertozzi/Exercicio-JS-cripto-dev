b5ec43d529a5aa5a31525abd655fd1bbfb7d5dc5
Bertozzi <veronica.bertozzi@accenture.com>
2022-04-02 11:49:10 -0300

    Exercicio JS 01

---
+++ js/fucao.js
@@ -0,0 +1,10 @@
+var nome;
+var idade;
+var email;
+nome = prompt("Digite o seu nome: ", "Digite o seu nome!");
+idade = prompt("Digite sua idade: ", "Digite sua idade!");
+email = prompt("Digite seu email: ", "Digite seu email!");
+// Criando uma função em JS 
+function dadosCliente(){
+    alert(nome+" tem " + idade + " anos e seu e-mail é "+email+"!");
+    }
